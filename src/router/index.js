7b6e44550f167ebd4267328a8111d4f0cc20c599
ophalvens <ophalvens@gmail.com>
2025-10-13 21:31:09 +0200

    demo reactiviteit en event-hooks in Ionic

--- src/router/index.js
+++ src/router/index.js
@@ -2,37 +2,37 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import TabsPage from '../views/TabsPage.vue'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
+	{
+		path: '/',
+		redirect: '/tabs/tabOverzicht'
+	},
+	{
+		path: '/tabs/',
+		component: TabsPage,
+		children: [
+			{
+				path: '',
+				redirect: '/tabs/tabOverzicht'
+			},
+			{
+				path: 'tabNieuw',
+				component: () => import('@/views/TabNieuwPage.vue')
+			},
+			{
+				path: 'tabOverzicht',
+				component: () => import('@/views/TabOverzichtPage.vue')
+			},
+			{
+				path: 'tabExperiment',
+				component: () => import('@/views/TabExperimentPage.vue')
+			}
+		]
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 })
 
 export default router
